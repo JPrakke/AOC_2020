27aea43b93832336513691374b232cfc6ec7625e
JPrakke <JoshPrakke@gmail.com>
2020-12-02 02:04:41 -0500

    removed useless print statements

--- 1/expense.py
+++ 1/expense.py
@@ -5,18 +5,13 @@ input_list = []
 
 with open(input , 'r') as reader:
     for line in reader.readlines():
-        print(f' loop: {line}')
         input_list.append(line)
-
-print(input_list)
-
 
 def challenge_1(input_list):
     for i in range(len(input_list)):
         for j in range (len(input_list)):
             if int(input_list[i]) + int(input_list[j]) == 2020:
                 print(int(input_list[i]) * int(input_list[j]))
-                print(f"itworked index: {i}, {j}") 
 
 def challenge_2(input_list):
     for i in range(len(input_list)):
@@ -24,6 +19,5 @@ def challenge_2(input_list):
             for ji in range (len(input_list)):
                 if int(input_list[i]) + int(input_list[j]) + int(input_list[ji]) == 2020:
                     print(int(input_list[i]) * int(input_list[j]) * int(input_list[ji]))
-                    print(f"itworked index: {i}, {j}, {ji}") 
 
-challenge_2(input_list)
+challenge_1(input_list)
